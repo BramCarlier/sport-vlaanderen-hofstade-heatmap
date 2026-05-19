aa8654bec86510784ead79ae09962653ee60c430
BramCarlier <bram-carlier@hotmail.com>
2026-05-19 19:43:27 +0200

    Configure Event model fields

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -2,9 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'latitude',
+        'longitude',
+        'weight',
+        'notes',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'latitude' => 'float',
+            'longitude' => 'float',
+            'weight' => 'integer',
+        ];
+    }
 }
