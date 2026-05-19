6e163dce24056224beff2926c0e8122b151f24a8
Bram Carlier <bram-carlier@hotmail.com>
2026-05-19 22:06:35 +0200

    enable all users for nova

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -54,9 +54,10 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     protected function gate(): void
     {
         Gate::define('viewNova', function (User $user) {
-            return in_array($user->email, [
-                //
-            ]);
+	    return true;
+	    //return in_array($user->email, [
+            //    //
+            //]);
         });
     }
 
