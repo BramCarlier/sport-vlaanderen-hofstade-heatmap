5d488df97942f0e44d0889eb1574b239dff5e336
BramCarlier <bram-carlier@hotmail.com>
2026-05-19 19:45:18 +0200

    Add heatmap web and API routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\EventController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [EventController::class, 'index'])->name('heatmap.index');
+
+Route::prefix('api')->group(function (): void {
+    Route::get('/events', [EventController::class, 'list'])->name('api.events.index');
+    Route::post('/events', [EventController::class, 'store'])->name('api.events.store');
+    Route::delete('/events/{event}', [EventController::class, 'destroy'])->name('api.events.destroy');
 });
